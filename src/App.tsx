cd8d91bf23ed231640fe202ae6030e004181c612
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-02-01 21:22:18 -0300

    feat: added styles components and button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <></>
+   <>
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+   </>
   )
 }
 
