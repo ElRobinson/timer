1713359666b1a2eb2041a66825ad33594e02026c
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-02-02 20:26:22 -0300

    feat: added default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { ThemeProvider } from 'styled-components'
 import { useState } from 'react'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
+   <ThemeProvider theme={defaultTheme}>
     <Button />
     <Button />
     <Button />
     <Button />
-   </>
+   </ThemeProvider>
   )
 }
 
