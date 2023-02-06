ff4e7b41501a1856f5b155552a93aa8378004c27
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-02-06 20:50:51 -0300

    feat: added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { useState } from 'react'
 import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <ThemeProvider theme={defaultTheme}>
-    <Button />
-    <Button />
-    <Button />
-    <Button />
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
 
-    <GlobalStyle />
-   </ThemeProvider>
-  ) 
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
-
-
