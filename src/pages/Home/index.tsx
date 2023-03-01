d6703feb1a82a0a86f7b904c04bde211d36b055c
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-02-28 21:15:44 -0300

    Feat: added react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,28 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdomnButton, TaskInput } from "./styles";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch('task');
+  const isSubmitedDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Work in</label>
           <TaskInput
            id="task"
            list="task-suggestions"
            placeholder="Project name"
+           {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -27,6 +39,7 @@ export function Home() {
            step={5}
            min={5}
            max={60}
+           {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -40,7 +53,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdomnButton disabled type="submit">
+        <StartCountdomnButton disabled={isSubmitedDisabled} type="submit">
           <Play size={24} />
           Start
         </StartCountdomnButton>
@@ -48,3 +61,4 @@ export function Home() {
     </HomeContainer>
   )
 }
+
